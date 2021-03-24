88cfc8f2c31085ba57fdc4ef386d60eac58e98f6
shashank bansal <16793896+shashban@users.noreply.github.com>
2021-03-24 10:27:08 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -15,7 +15,6 @@ test('wait 500 ms', async () => {
   expect(delta).toBeGreaterThanOrEqual(500);
 });
 
-// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = 500;
   const ip = path.join(__dirname, 'index.js');
